af8019a8d992ad11225d8ea348eff29c65f7633d
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2018-05-10 07:25:44 +0800

    fix: min canvas must be 1x1; fix list comprehensions

--- lib/canvas.ex
+++ lib/canvas.ex
@@ -7,22 +7,21 @@ defmodule DrawingApp.Canvas do
   @enforce_keys [:width, :height, :coordinates]
   defstruct [:width, :height, :coordinates]
 
-  @canvas_range 0..50
+  @canvas_range 1..50
 
   def new(width \\ 50, height \\50)
   def new(width, height) when width in(@canvas_range) and height in(@canvas_range) do
     # { :ok, %Canvas{width: width, height: height, view: draw_canvas(width, height)} }
     width_at_boundary = width + 1
-    height_at_boundary = height
+    height_at_boundary = height + 1
     coordinates =
-      # for y <- 1..height_at_boundary, x <- 0..width_at_boundary, into: MapSet.new() do
-      for x <- 0..width_at_boundary, y <- 1..height_at_boundary, into: MapSet.new() do
+      for y <- 0..height_at_boundary, x <- 0..width_at_boundary, into: MapSet.new() do
         cond do
-          ((y == 1 || y == height_at_boundary) && x == width_at_boundary)             -> {:ok, coord} = Coordinate.new(x, y, "-\n"); coord
-          ((x == 0 || x == width_at_boundary) && (y != 1 || y != height_at_boundary)) -> {:ok, coord} = Coordinate.new(x, y, "|"); coord
-          y == 1 || y == height_at_boundary                                           -> {:ok, coord} = Coordinate.new(x, y, "-"); coord
-          x == width_at_boundary                                                      -> {:ok, coord} = Coordinate.new(x, y, "|\n"); coord
-          true                                                                        -> {:ok, coord} = Coordinate.new(x, y); coord
+          ((y == 0 || y == height_at_boundary) && x == width_at_boundary)        -> {:ok, coord} = Coordinate.new(x, y, "-\n"); coord
+          ((y != 0 || y != height_at_boundary) && x == width_at_boundary)        -> {:ok, coord} = Coordinate.new(x, y, "|\n"); coord
+          (x == 0 && y != 0 && y != height_at_boundary)                          -> {:ok, coord} = Coordinate.new(x, y, "|"); coord
+          (y == 0 || y == height_at_boundary)                                    -> {:ok, coord} = Coordinate.new(x, y, "-"); coord
+          true                                                                   -> {:ok, coord} = Coordinate.new(x, y); coord
         end
       end
     { :ok, %Canvas{width: width, height: height, coordinates: coordinates} }
