20de91277e0dd691652b057a99d58c57a7fb9b97
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2018-05-09 18:59:19 +0800

    feat: list comprehend coords into MapSet

--- lib/canvas.ex
+++ lib/canvas.ex
@@ -7,15 +7,23 @@ defmodule DrawingApp.Canvas do
   @enforce_keys [:width, :height, :coordinates]
   defstruct [:width, :height, :coordinates]
 
-  @canvas_range 1..50
+  @canvas_range 0..50
 
   def new(width \\ 50, height \\50)
   def new(width, height) when width in(@canvas_range) and height in(@canvas_range) do
     # { :ok, %Canvas{width: width, height: height, view: draw_canvas(width, height)} }
+    width_at_boundary = width + 1
+    height_at_boundary = height
     coordinates =
-      for x <- 1..width, y <- 1..height do
-        {:ok, coord} = Coordinate.new(x,y)
-        coord
+      # for y <- 1..height_at_boundary, x <- 0..width_at_boundary, into: MapSet.new() do
+      for x <- 0..width_at_boundary, y <- 1..height_at_boundary, into: MapSet.new() do
+        cond do
+          ((y == 1 || y == height_at_boundary) && x == width_at_boundary)             -> {:ok, coord} = Coordinate.new(x, y, "-\n"); coord
+          ((x == 0 || x == width_at_boundary) && (y != 1 || y != height_at_boundary)) -> {:ok, coord} = Coordinate.new(x, y, "|"); coord
+          y == 1 || y == height_at_boundary                                           -> {:ok, coord} = Coordinate.new(x, y, "-"); coord
+          x == width_at_boundary                                                      -> {:ok, coord} = Coordinate.new(x, y, "|\n"); coord
+          true                                                                        -> {:ok, coord} = Coordinate.new(x, y); coord
+        end
       end
     { :ok, %Canvas{width: width, height: height, coordinates: coordinates} }
   end
--- lib/coordinate.ex
+++ lib/coordinate.ex
@@ -4,8 +4,8 @@ defmodule DrawingApp.Coordinate do
   @enforce_keys [:x, :y, :val]
   defstruct [:x, :y, :val]
 
-  def new(x, y, val \\ "")
-  def new(x, y, val) when x > 0 and y > 0, do:
+  def new(x, y, val \\ " ")
+  def new(x, y, val) when x >= 0 and y >= 0, do:
     {:ok, %Coordinate{x: x, y: y, val: val}}
 
   def new(_x, _y, _val), do: {:error, :invalid_coordinate}
